82a2f52ea5cca82c675603aa09557985ad0a3c7b
L0uh3n <apenaslh@gmail.com>
2023-12-01 11:25:40 -0300

    att 1.1: fixing bugs

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Extensão criada com o intuito de me auxiliar a conseguir os pontos diários de 
 
 ## Testando a extensão:
 https://github.com/L0uh3n/auto_search/assets/86615869/22945e98-0ab0-41e5-8124-c00826db006d
+
+# Atualizações:
+* Versão 1.1 ->  Agora existe um cooldown para inserir a palavra gerada no campo de busca e posterioremnte executar a pesquisa. Foi necessário fazer isso pois de alguma forma os pontos não estavam aumentando, essa foi uma solução rápida que encontrei.
--- content.js
+++ content.js
@@ -15,11 +15,7 @@ const tamanhoPalavra = 8;
 const palavraAleatoria = gerarPalavraAleatoria(tamanhoPalavra);
 console.log(palavraAleatoria);
 
-// Função que insere as palavras geradas na caixa de texto e pesquisa automaticamente.
-function autoSearch() {
-    const searchBar = document.getElementsByTagName("textarea");
-    const searchEnter = document.getElementsByTagName("input");
-
+function inserePalavraAleatoria(palavraAleatoria) {
     for (let i = 0; i < searchBar.length; i++) {
         console.log(searchBar[i].id);
         if (searchBar[i].id.toLowerCase().includes("sb_form_q")) {
@@ -35,4 +31,27 @@ function autoSearch() {
     }
 }
 
-setTimeout(autoSearch, 3000);
+// Função que insere as palavras geradas na caixa de texto e pesquisa automaticamente.
+function autoSearch() {
+    const searchBar = document.getElementsByTagName("textarea");
+    const searchEnter = document.getElementsByTagName("input");
+
+    // Att: foi necessário colocar um cooldown para inserir a palavra gerada no campo de buscar e posterioremnte executar.
+    setTimeout(function() {
+        for (let i = 0; i < searchBar.length; i++) {
+            console.log(searchBar[i].id);
+            if (searchBar[i].id.toLowerCase().includes("sb_form_q")) {
+                searchBar[i].value = palavraAleatoria;
+    
+                for (let j = 0; j < searchEnter.length; j++) {
+                    console.log(searchEnter[j].id);
+                    if (searchEnter[j].id.toLowerCase().includes("sb_form_go")) {
+                        searchEnter[j].click();
+                    }
+                }
+            } 
+        }
+    }, 2500);
+}
+
+setTimeout(autoSearch, 5000);
