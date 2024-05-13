85111e2d02e029c8da36ca2c3700ee133b1e647b
L0uh3n <apenaslh@gmail.com>
2024-05-12 23:38:08 -0300

    atualizando extensao apos mudancas do bing

--- content.js
+++ content.js
@@ -1,57 +1,106 @@
 // Função que gera as palavras aleatórias.
-function gerarPalavraAleatoria(tamanho) {
-    const caracteres = 'abcdefghijklmnopqrstuvwxyz';
-    let palavra = '';
+function gerarPalavraAleatoria() {
+    const palavrasAleatorias = [
+        "abacaxi",
+        "bicicleta",
+        "cachorro",
+        "diamante",
+        "elefante",
+        "foguete",
+        "girassol",
+        "hipopótamo",
+        "igreja",
+        "jacaré",
+        "kiwi",
+        "limonada",
+        "macarrão",
+        "nariz",
+        "ombrelone",
+        "panda",
+        "quadro",
+        "raposa",
+        "sapato",
+        "tigre",
+        "uva",
+        "violão",
+        "xadrez",
+        "yoga",
+        "zebra",
+        "abóbora",
+        "borboleta",
+        "carambola",
+        "dado",
+        "escada",
+        "foca",
+        "gato",
+        "hamburguer",
+        "índio",
+        "jardim",
+        "ketchup",
+        "leão",
+        "mochila",
+        "navio",
+        "óculos",
+        "pente",
+        "queijo",
+        "roda",
+        "sol",
+        "tatuagem",
+        "urso",
+        "viagem",
+        "waffle",
+        "xícara",
+        "yogurte",
+        "zoológico",
+        "anel",
+        "baleia",
+        "camisa",
+        "dente",
+        "escova",
+        "fogueira",
+        "globo",
+        "hamster",
+        "ilha",
+        "janela",
+        "karaokê",
+        "lua",
+        "meia",
+        "navalha",
+        "orelha",
+        "pipoca",
+        "queimadura",
+        "rosto",
+        "sol",
+        "tesoura",
+        "urso",
+        "ventilador",
+        "xícara",
+        "yo-yo",
+        "zíper"
+    ];
 
-    for (let i = 0; i < tamanho; i++) {
-        const indice = Math.floor(Math.random() * caracteres.length);
-        palavra += caracteres[indice];
+    for (let i = 0; i < palavrasAleatorias.length; i++) {
+        var indice = Math.floor(Math.random() * palavrasAleatorias.length);
     }
 
-    return palavra;
+    return palavrasAleatorias[indice];
 }
 
-const tamanhoPalavra = 8;
-const palavraAleatoria = gerarPalavraAleatoria(tamanhoPalavra);
+var palavraAleatoria = gerarPalavraAleatoria();
 console.log(palavraAleatoria);
 
-function inserePalavraAleatoria(palavraAleatoria) {
-    for (let i = 0; i < searchBar.length; i++) {
-        console.log(searchBar[i].id);
-        if (searchBar[i].id.toLowerCase().includes("sb_form_q")) {
-            searchBar[i].value = palavraAleatoria;
-
-            for (let j = 0; j < searchEnter.length; j++) {
-                console.log(searchEnter[j].id);
-                if (searchEnter[j].id.toLowerCase().includes("sb_form_go")) {
-                    searchEnter[j].click();
-                }
-            }
-        } 
-    }
-}
-
 // Função que insere as palavras geradas na caixa de texto e pesquisa automaticamente.
-function autoSearch() {
-    const searchBar = document.getElementsByTagName("textarea");
-    const searchEnter = document.getElementsByTagName("input");
+function inserePalavraAleatoria() {
+    const searchBar = document.getElementById("sb_form_q");
+    const searchEnter = document.getElementById("sb_form_go");
+    console.log(searchBar);
 
-    // Att: foi necessário colocar um cooldown para inserir a palavra gerada no campo de buscar e posterioremnte executar.
-    setTimeout(function() {
-        for (let i = 0; i < searchBar.length; i++) {
-            console.log(searchBar[i].id);
-            if (searchBar[i].id.toLowerCase().includes("sb_form_q")) {
-                searchBar[i].value = palavraAleatoria;
+    searchBar.value = palavraAleatoria;
     
-                for (let j = 0; j < searchEnter.length; j++) {
-                    console.log(searchEnter[j].id);
-                    if (searchEnter[j].id.toLowerCase().includes("sb_form_go")) {
-                        searchEnter[j].click();
-                    }
-                }
-            } 
-        }
-    }, 2500);
+    // Criei um intervalo para realizar a pesquisa.
+    setInterval(() => {
+        searchEnter.click();
+    }, 1000);
 }
 
-setTimeout(autoSearch, 5000);
+setTimeout(inserePalavraAleatoria, 5000);
